a69d1626cee65066cd0a5f918440c381f5c27a33
gokel166 <okellogeorge40@yahoo.com>
2018-02-12 02:05:09 -0600

    utilize httpoison api and html parser for get methods

--- web/models/page_analyzer.ex
+++ web/models/page_analyzer.ex
@@ -1,7 +1,26 @@
 defmodule BrokenLinks.PageAnalyzer do
     # returns a list of broken urls
+    #1. Allow user to enter a url
+    #2. Parse out the links from (1)
+    #3. Try to download html for the links from (2)
+    #   if it fails for a link, it means that the link is broken
     def analyze(url) do
-        #
-        [%{text: "Yooer", href: "Yooer.com"}]
+        #Download html of url
+        case HTTPoison.get(url, [], [ ssl: [{:versions, [:'tlsv1.2']}] ]) do
+            {:ok, %HTTPoison.Response{body: body}} ->
+                #2. Parse out links from (1)
+              links = parse_links(body)
+              {:ok, links}
+            oops ->
+                {:error, inspect(oops)}
+        end
+        [%{text: "Random", href: "random.com"}]
+    end
+
+    defp parse_links(html) do
+        Floki.find(html, "a")
+        |> Enum.map(fn {_, attrs, [text]} ->
+            {_ , href} = Enum.find(attrs, fn {k, v} k == "href" end)
+        end)
     end
 end
